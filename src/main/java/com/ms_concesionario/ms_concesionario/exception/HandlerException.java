91ce492620c0e79862ca0d4ecced09e203fb262d
unknown <balles07.yb@gmail.com>
2024-07-08 17:33:17 -0500

    Control de argumentos no validos

--- src/main/java/com/ms_concesionario/ms_concesionario/exception/HandlerException.java
+++ src/main/java/com/ms_concesionario/ms_concesionario/exception/HandlerException.java
@@ -3,8 +3,13 @@ package com.ms_concesionario.ms_concesionario.exception;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @RestControllerAdvice
@@ -13,5 +18,15 @@ public class HandlerException {
     @ExceptionHandler(CarNotFoundException.class)
     public ResponseEntity<?> handleCarNotFound(CarNotFoundException ex){
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<?>> handleValidateException(MethodArgumentNotValidException ex){
+        List<String> errors = new ArrayList<>();
+        for (FieldError error: ex.getBindingResult().getFieldErrors()){
+            String er = error.getField() + " : " + error.getDefaultMessage();
+            errors.add(er);
+        }
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
